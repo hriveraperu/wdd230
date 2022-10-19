53d488576704815408ebe43d1e590005cea2a344
hriveraperu <gioxshaoran@hotmail.com>
2022-10-19 15:17:45 -0500

    W05 =)

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -19,6 +19,44 @@ function toggleMenu() {
     document.getElementById("hamburger").classList.toggle("open");
 }
 
-const x = document.getElementById("hamburger")
+const x = document.getElementById("hamburger");
 
-x.onclick = toggleMenu
+x.onclick = toggleMenu;
+
+const bannerWeek = document.createElement("p");
+
+bannerWeek.textContent = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+
+const actualDate = new Date();
+let dayWeek = actualDate.getDay();
+
+
+
+if (dayWeek >= 1 && dayWeek <=2 ) {
+    document.querySelector("header").insertBefore(bannerWeek, document.querySelector("#header"));
+    };
+
+
+let weather = {
+    apiKey: 'YYFSSFGGBPPBEFWVYA6FKNF35',
+    fetchWeather: function () {
+        fetch(
+            "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/15081%2C%20Lima%2C%20Peru/today?unitGroup=metric&key=YYFSSFGGBPPBEFWVYA6FKNF35&contentType=json"
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { temp, conditions, humidity, icon } = data.currentConditions;
+        const { description } = data;
+        console.log(temp, conditions, humidity, icon, description)
+        document.querySelector(".temperature").innerHTML = temp + '°';
+        document.querySelector(".humidity").innerHTML = humidity + "%";
+        document.querySelector(".temp-icon").src = 'https://www.visualcrossing.com/img/' + icon + ".svg";
+        document.querySelector(".conditions").innerHTML = conditions;
+        document.querySelector(".temp-description").innerHTML = description;
+
+    }
+};
+
+weather.fetchWeather();
