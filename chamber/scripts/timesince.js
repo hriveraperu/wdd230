2d97242416b3521c096b375d4a37b041c55ec443
hriveraperu <gioxshaoran@hotmail.com>
2022-11-05 00:01:49 -0500

    =)

--- chamber/scripts/timesince.js
+++ chamber/scripts/timesince.js
@@ -7,7 +7,7 @@ let numVisits = Number(window.localStorage.getItem("visits-ls"));
 if (numVisits !== 0) {
 	localStorage.setItem("visit2", newStamp)
     daynum = parseInt(newStamp) - parseInt(timeStamp)
-    days = Math.floor(daynum / (24*60*60*1000));
+    days = Math.ceil(daynum / (24*60*60*1000));
     visitsDisplay.textContent = days;
 } else {
     visitsDisplay.textContent = "This is your first visit!";
